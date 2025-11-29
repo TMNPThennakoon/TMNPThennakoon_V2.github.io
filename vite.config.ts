474ba879044fd087f9607ed7224d5fbb8b373de1
TMNPThennakoon <napi@Nayanas-MacBook-Air.local>
2025-11-29 13:07:17 +0530

    Update base path for custom domain deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/TMNP.Thennakoon_V2/",
+  base: "/",
   plugins: [
     react(),
     tailwindcss(),
